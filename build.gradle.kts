52c614008de1658a544d6576d510f69708907104
QwQ3094-Live <caonimen587@gmail.com>
2025-04-04 13:36:24 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,3 @@ plugins {
     alias(libs.plugins.com.google.firebase.crashlytics) apply false
     alias(libs.plugins.com.google.firebase.firebase.pref) apply false
 }
-
-task<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
